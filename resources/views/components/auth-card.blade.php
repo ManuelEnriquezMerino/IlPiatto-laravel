0562c67292b98d08cd121b83541eadc25a41f7da
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-04-27 17:18:47 -0300

    Cambios en ventana de logueo
    *Agregado boton para regresar al inicio
    *Cambiado color de fondo, cuadro de ingreso de datos y logo
    *Modificado el layout de la pagina

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,15 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-royalblue-3">
-    <div>
-        {{ $logo }}
+<div class="min-h-screen bg-cloud-gray">
+    <div class="p-2">
+            {{ $btnInicio }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+    <div class="flex flex-col sm:justify-center items-center pt-3 sm:pt-0 ">
+        <div>
+            {{ $logo }}
+        </div>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-light-gray shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
     </div>
 </div>
