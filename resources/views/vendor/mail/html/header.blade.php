9403e2c5b29a2624c380a1a5158a2dbebbd90446
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-04-27 20:48:58 -0300

    Agregado mensaje de correo de recuperacion de contraseña personalizado

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="https://drive.google.com/uc?id=1tL7bTANs1Gtij5u1itrS8FfETioXhESX" class="logo" alt="Il Piatto">
+        </a>
+    </td>
+</tr>
