f978ff46faa870620ecc997c0dbf222a2c26be44
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-04-28 13:08:28 -0300

    Modificada pagina de confirmacion
    *Modificado texto a español

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,18 +1,16 @@
 <x-default-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('Antes de poder ingresar al sitio deberá verificar su correo electrónico mediante el enlace provisto en un correo que le acabamos de enviar, en caso de que no haya recibido el correo puede hacer click en el siguiente boton.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                {{ __('Se ha enviado un nuevo correo') }}
             </div>
         @endif
 
@@ -22,7 +20,7 @@
 
                 <div>
                     <x-button>
-                        {{ __('Resend Verification Email') }}
+                        {{ __('Reenviar correo') }}
                     </x-button>
                 </div>
             </form>
@@ -31,7 +29,7 @@
                 @csrf
 
                 <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
+                    {{ __('Cerrar sesión') }}
                 </button>
             </form>
         </div>
