25c3cdd1dd072b4b4eeae741583726c43707dc2f
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-04-30 18:55:47 -0300

    Agregados modelos

---
+++ app/Models/restriccion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class restriccion extends Model
+{
+    use HasFactory;
+}
