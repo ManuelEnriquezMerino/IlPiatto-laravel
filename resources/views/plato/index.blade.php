950c3a4b953987731c281c1d125fbb2866382759
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-05-15 12:26:34 -0300

    Agregado "DataTables" a la tabla index
    Agregado paginado, busqueda y ordenamiento

--- resources/views/plato/index.blade.php
+++ resources/views/plato/index.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.plantilla')
 
-@section('contenido')
-<a href="platos/create" class="btn btn-primary">CREAR</a>
+@section('css')
+    <link href="https://cdn.datatables.net/1.12.0/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+@endsection
 
-<table class="table table-dark table-stripped mt-4">
-    <thead>
+@section('contenido')
+<a href="platos/create" class="btn btn-primary mb-2">CREAR</a>
+
+<table id="platos" class="table table-striped shadow-lg mt-4" style="width:100%">
+    <thead class="bg-primary text-white">
         <tr>
             <th scope="col">ID</th>
             <th scope="col">Nombre</th>
@@ -35,4 +39,23 @@
     </tbody>
 
 </table>
+
+@section('js')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap5.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('#platos').DataTable({
+                "lengthMenu": [[5,10,50,-1],[5,10,50,"Todos"]],
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/1.12.0/i18n/es-ES.json"
+                }
+                
+            });
+        });
+    </script>
+@endsection
+
 @endsection
