384c14a7e702f63cb8069dcd9ec3bc928da19c86
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-05-20 15:33:50 -0300

    Agregados seeders
    Seeders para:
         * Categorias
         * Restricciones
         * Opcionales
         * Categoria - Plato
         * Plato - Restriccion

---
+++ database/seeders/CategoriasSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Categoria;
+
+class CategoriasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Categoria::create([
+            'id' => '1',
+            'nombre' => 'Carnes',
+            'descripcion' => 'Platos basados en distintos tipos de carnes'
+        ]);
+
+        Categoria::create([
+            'id' => '2',
+            'nombre' => 'Pastas',
+            'descripcion' => 'Platos basados en Pastas'
+        ]);
+    }
+}
---
+++ database/seeders/RestriccionesSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Restriccion;
+
+class RestriccionesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Restriccion::create([
+            'id' => '1',
+            'nombre' => 'Vegano',
+            'descripcion' => 'Platos que no contienen derivados de animales'
+        ]);
+
+        Restriccion::create([
+            'id' => '2',
+            'nombre' => 'Celiaco',
+            'descripcion' => 'Platos que solo contienen alimentos sin TACC'
+        ]);
+    }
+}
